7bb11840993e16253a8cba3a5edd801d99627e09
Joshua Simon <39858834+JoshuaSimon@users.noreply.github.com>
2019-08-08 23:32:16 +0200

    Fixed simpson's rule.

--- Python/Numerical_Integration.py
+++ Python/Numerical_Integration.py
@@ -10,16 +10,20 @@ def g (x):
 def h (x):
   return np.sin(x)
 
+# Test function 3
+def k (x):
+  return 3**(3*x-1)
+
 # Composite Simpson's rule
 def com_simpson(function, a, b, n):
   sum1 = 0.0
   h = (b-a)/n
 
-  for k in range(0,n):
+  for k in range(1,n+1):
     x_k = a + k * h
-    x_k1 = a + (k + 1) * h
+    x_k1 = a + (k - 1) * h
 
-    simpson = h/6 * ( function(x_k) + 4* function((x_k + x_k1)/2) + function(x_k1))
+    simpson = h/6 * ( function(x_k1) + 4* function((x_k1 + x_k)/2) + function(x_k))
 		
     sum1 = sum1 + simpson
 
@@ -51,6 +55,10 @@ integral_g_2 = trapez(g, interval_a, interval_b, n)
 integral_h_1 = com_simpson(h, 0.00, 3.141592654/2.0, 400)
 integral_h_2 = trapez(h, 0.00, 3.141592654/2.0, 100)
 
+# Example 3:
+integral_k_1 = com_simpson(k, 0.00, 2.0, 3)
+integral_k_2 = trapez(k, 0.00, 2.0, 3)
+
 # Output
 print("Integral of g(x) over [", interval_a, " , ", interval_b, "]")
 print("Simpson: ", integral_g_1)
@@ -62,3 +70,9 @@ print("Integral of g(x) over [0 , pi/2]")
 print("Simpson: ", integral_h_1)
 print("Trapez: ", integral_h_2)
 print("Exact value is ", -1 * np.cos(3.141592654/2.) + np.cos(0.))
+print()
+
+print("Integral of k(x) over [0 , 2]")
+print("Simpson: ", integral_k_1)
+print("Trapez: ", integral_k_2)
+print("Exact value is ", 728/(9 * np.log(3)))
