c7b45669e176cae56015c9c97c44f21a51f00040
JoshuaSimon <joshsimon96@googlemail.com>
2022-02-13 20:12:15 +0100

    Updated code format a bit

--- C++/Numerical_Integration.cpp
+++ C++/Numerical_Integration.cpp
@@ -1,13 +1,13 @@
-/* Numerical_Integration.cpp */
-/* -------------------------------------------
+/* 
+	Numerical_Integration.cpp
+	-------------------------------------------
 	Created by Joshua Simon.
 	Date: 09.07.2018
- ------------------------------------------- */
+ 	------------------------------------------- 
+ */
+
 #include <iostream>
 #include <cmath>
-using namespace std;
-
-/* Functions */
 
 // Create some functions you want to integrate like this:
 double g (double x) {
@@ -33,10 +33,10 @@ double trapez (double f(double), double a, double b, int n);
 
 
 /* Main */
-int main (void) {
-	
-	double interval_a = 0.0;		// lower interval bound
-	double interval_b = 4.0;		// upper interval bound
+int main (void) 
+{
+	double interval_a = 0.0;	// lower interval bound
+	double interval_b = 4.0;	// upper interval bound
 	int n = 100;				// number of subintervals
 	
 	// Call functions to calculate integral value
@@ -53,36 +53,36 @@ int main (void) {
 	double integral_k_2 = trapez(k, 0.0, 2.0, 3);
 	
 	// Output
-	cout << "Integral of g(x) over [" << interval_a << "," << interval_b << "] " << endl;
-	cout << "Simpson: " << integral_g_1 << endl;
-	cout << "Trapez:  " << integral_g_2 << endl;
-	cout << "Exact value is " << 1./3. * (4.*4.*4.) << " ." << endl << endl;
+	std::cout << "Integral of g(x) over [" << interval_a << "," << interval_b << "] " << std::endl;
+	std::cout << "Simpson: " << integral_g_1 << std::endl;
+	std::cout << "Trapez:  " << integral_g_2 << std::endl;
+	std::cout << "Exact value is " << 1./3. * (4.*4.*4.) << " ." << std::endl << std::endl;
 	
-	cout << "Integral of h(x) over [0,pi/2] " << endl;
-	cout << "Simpson: " << integral_h_1 << endl;
-	cout << "Trapez:  " << integral_h_2 << endl;
-	cout << "Exact value is " << -cos(3.141592654/2.) + cos(0.) << " ." << endl << endl;
+	std::cout << "Integral of h(x) over [0,pi/2] " << std::endl;
+	std::cout << "Simpson: " << integral_h_1 << std::endl;
+	std::cout << "Trapez:  " << integral_h_2 << std::endl;
+	std::cout << "Exact value is " << -cos(3.141592654/2.) + cos(0.) << " ." << std::endl << std::endl;
 	
-	cout << "Integral of k(x) over [0, 2] " << endl;
-	cout << "Simpson: " << integral_k_1 << endl;
-	cout << "Trapez:  " << integral_k_2 << endl;
-	cout << "Exact value is " << 728 / (9 * log(3)) << " ." << endl << endl;
+	std::cout << "Integral of k(x) over [0, 2] " << std::endl;
+	std::cout << "Simpson: " << integral_k_1 << std::endl;
+	std::cout << "Trapez:  " << integral_k_2 << std::endl;
+	std::cout << "Exact value is " << 728 / (9 * log(3)) << " ." << std::endl << std::endl;
 	
 	system("Pause");
 }
 
-// Composite Simpson's rule
-double com_simpson(double f(double), double a, double b, int n) {
-
-	// Integration of function f over interval [a,b] where n is
-	// the number of subintervals. Accuracy only depends on the 
-	// number of subintervals.
+// Composite Simpson's rule.
+// Integration of function f over interval [a,b] where n is
+// the number of subintervals. Accuracy only depends on the 
+// number of subintervals.
+double com_simpson(double f(double), double a, double b, int n) 
+{
 	double h, x_k, x_k1, simpson;
 	double sum = 0.0;
 
 	if (a > b)
 	{
-		cerr << "ERROR: Lower interval bound a has to be smaller than upper bound b." << endl;
+		std::cerr << "ERROR: Lower interval bound a has to be smaller than upper bound b." << std::endl;
 		return 0;
 	}
 
@@ -94,28 +94,26 @@ double com_simpson(double f(double), double a, double b, int n) {
 	{
 		x_k = a + k * h;
 		x_k1 = a + (k - 1.0) * h;
-
 		simpson = h / 6 * (f(x_k1) + 4 * f((x_k1 + x_k) / 2) + f(x_k));
-
 		sum = sum + simpson;
 	}
 
 	return sum;
 }
 
-// Trapezoidal rule
-double trapez (double f(double), double a, double b, int n) {
-	
-	// Integration of function f over interval [a,b] where n is
-	// the number of subintervals. Accuracy only depends on the 
-	// number of subintervals. The trapezoidal rule converges 
-	// rapidly for periodic functions.
+// Trapezoidal rule.
+// Integration of function f over interval [a,b] where n is
+// the number of subintervals. Accuracy only depends on the 
+// number of subintervals. The trapezoidal rule converges 
+// rapidly for periodic functions.
+double trapez (double f(double), double a, double b, int n) 
+{
 	double h;
 	double sum = 0.00;
 	
 	if(a > b)
 	{
-		cerr << "ERROR: Lower interval bound a has to be smaller than upper bound b." << endl;
+		std::cerr << "ERROR: Lower interval bound a has to be smaller than upper bound b." << std::endl;
 		return 0;
 	}
 	
